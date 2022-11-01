112d510daec8b95b86b8586c4ac7cf071a3b3cfe
Vasu Kansal <vasu12b8075@gmail.com>
2022-11-01 10:00:27 +0530

    Star Square

--- Pattern Questions/Star_Square.java
+++ Pattern Questions/Star_Square.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Square {
+public class Star_Square {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int rows;
