9a1dbf3db386dd5aa7dd5a93cc67ef7322742b58
Vasu Kansal <vasu12b8075@gmail.com>
2022-11-01 09:59:14 +0530

    Inverted Star Triangle

---
+++ Pattern Questions/Inverted_Triangle.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Inverted_Triangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rows;
+        System.out.print("ENter the no. of rows - ");
+        rows = sc.nextInt();
+        sc.close();
+        pat(rows);
+    }
+
+    public static void pat(int rows) {
+        int count = rows;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= count; j++) {
+                System.out.print("*");
+
+            }
+            count--;
+            System.out.println();
+        }
+    }
+}
