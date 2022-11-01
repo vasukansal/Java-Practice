6729981c2a805b035b924c31a37f01e1a70c02f2
Vasu Kansal <vasu12b8075@gmail.com>
2022-11-01 09:38:48 +0530

    Triangle

---
+++ Pattern Questions/Triangle.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Triangle {
+    public static void main(String[] args) { // Triangle pattern
+        Scanner sc = new Scanner(System.in);
+        int row;
+        System.out.print("Enter the no. of Rows - ");
+        row = sc.nextInt();
+        sc.close();
+        pat(row);
+    }
+
+    public static void pat(int row) {
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
